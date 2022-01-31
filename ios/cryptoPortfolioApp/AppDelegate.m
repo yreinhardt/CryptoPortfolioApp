6f1e29c7a431f017bf6d7ea921b96e94856488d6
yreinhardt <mail.yannick.reinhardt@gmail.com>
2022-01-31 21:39:48 +0100

    update firebase

--- ios/cryptoPortfolioApp/AppDelegate.m
+++ ios/cryptoPortfolioApp/AppDelegate.m
@@ -28,9 +28,8 @@ static void InitializeFlipper(UIApplication *application) {
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   // initialize Firebase SDK
-  if ([FIRApp defaultApp] == nil) {
   [FIRApp configure];
-  }
+  
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
