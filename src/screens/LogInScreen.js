5ae243045bf88d1bc1fd086e6ef4f78374f80f0b
yreinhardt <mail.yannick.reinhardt@gmail.com>
2022-01-31 12:54:27 +0100

    work on firbase auth

--- src/screens/LogInScreen.js
+++ src/screens/LogInScreen.js
@@ -1,18 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 import { TextInput, KeyboardAvoidingView, View, TouchableOpacity, StyleSheet, Text } from "react-native";
 
 
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-community/google-signin';
+
+const CLIENT_ID = process.CLIENT_ID
+
+GoogleSignin.configure({
+  webClientId: CLIENT_ID,
+});
+
+
 const LogInScreen = ({ navigation }) => {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+
   return(
 
 
     <KeyboardAvoidingView style={styles.container} behavior="padding">
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Email" style={styles.input}/>
-        <TextInput placeholder="Password" style={styles.input}/>
+        <TextInput 
+          placeholder="Email" 
+          style={styles.input} 
+          value = {email} 
+          onChangeText={text => setEmail(text)}/>
+        <TextInput 
+          placeholder="Password"
+          secureTextEntry 
+          style={styles.input} 
+          value = {password} 
+          onChangeText={text => setPassword(text)}/>
       </View>
 
       <View style={styles.buttonContainer}>
+      <GoogleSigninButton
+        style={{ width: 192, height: 48 }}
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+     />
+
         <TouchableOpacity style={[styles.button]} onPress={() => {}}>
           <Text style={styles.buttonText}>Log in</Text>
         </TouchableOpacity>
@@ -56,6 +89,15 @@ const styles = StyleSheet.create({
     padding: 15,
     borderRadius: 5,
     alignItems: 'center'
+
+  },
+  buttonGoogle:{
+    backgroundColor: "green",
+    width: '100%',
+    padding: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginBottom:5,
 
   },
   buttonOutline:{
